b9b9f43c8cb506a64079abc34edbb8228694c123
LPM <11pm@LPMs-Air.fios-router.home>
2022-10-14 17:15:01 -0400

    billy bass pro shop

--- client/index.js
+++ client/index.js
@@ -8,7 +8,7 @@ import App from './app';
 import {createRoot} from 'react-dom/client';
 import './socket';
 
-const container = document.getElementById('app');
+const container = document.getElementById('billy-bass-pro-shop');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Provider store={store}>
