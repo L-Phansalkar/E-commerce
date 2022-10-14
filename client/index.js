24a42c4214f87d879676c535b9bc52bac8e05a31
LPM <11pm@LPMs-Air.fios-router.home>
2022-10-14 15:03:47 -0400

    new render"

--- client/index.js
+++ client/index.js
@@ -5,6 +5,10 @@ import {Router} from 'react-router-dom';
 import history from './history';
 import store from './store';
 import App from './app';
+import {createRoot} from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
 
 // establishes socket connection
 import './socket';
